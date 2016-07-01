8bbd0051f6481e90fb0e88b3d3a5b8aeb5105ce2
Alireza Balouch <gswape@gmail.com>
2016-07-01 23:05:40 +0200

    break it up

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,22 @@ const $ = require('gulp-load-plugins')();
 
 const buildPath = 'dist/';
 
-gulp.task('minify', function () {
-	return gulp.src(['src/sjs.js'])
-		.pipe($.babel())
+gulp.task('minify', ['babel'], function () {
+	return gulp.src(['dist/sjs.js'])
 		.pipe($.uglifyjs('sjs.min.js'))
 		.pipe(gulp.dest(buildPath));
 });
 
 gulp.task('babel', function () {
 	return gulp.src(['src/sjs.js'])
-		.pipe($.babel())
+		.pipe($.babel({
+			presets: ['es2015']
+		}))
 		.pipe(gulp.dest(buildPath));
 });
 
-gulp.task('default', ['minify', 'babel']);
+gulp.task('default', ['watch']);
 
 gulp.task('watch', function () {
-	gulp.watch(['src/sjs.js'], ['minify', 'babel']);
+	gulp.watch(['src/sjs.js'], ['minify']);
 });
